f6922caa0af956f54f51a6564de2b7987cc0e7a4
Jivak Sute <112770989+jivakys@users.noreply.github.com>
2022-10-19 18:46:37 +0530

    Update prime.js
    I will fix your prime code now code is correct.

--- prime.js
+++ prime.js
@@ -3,9 +3,10 @@ let count = 0;
 for(let i=1; i<=num; i++){
     if(num % i == 0){
         count++;
-    }if(count==2){
+    }
+}
+    if(count==2){
         console.log("it is prime")
     }else{
         console.log("it is not prime")
     }
-}
