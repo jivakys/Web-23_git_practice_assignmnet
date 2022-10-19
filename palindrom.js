4ffcaf3118928587400e9fee5b85fb7baeb4173b
Jivak Sute <112770989+jivakys@users.noreply.github.com>
2022-10-19 18:44:25 +0530

    Update palindrom.js
    I will correct your code mistake. now the code is correct.

--- palindrom.js
+++ palindrom.js
@@ -3,10 +3,10 @@ let str = "naman";
 let new_str = ""
 
 for(let i=str.length; i>0; i--){
-    new_str = new_str + str[i]+" ";
+    new_str = new_str + str[i];
 
 }if(new_str==str){
-    console.log("it is plaindrome")
+    console.log("it is palindrome")
 }else{
-    console.log("it is not plaindrome")
+    console.log("it is not palindrome")
 }
