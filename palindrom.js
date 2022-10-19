321ae05cdff4f2175d42917075fe1c7ce6f8f7a7
jay partike <partikejayu@gmail.com>
2022-10-19 18:22:24 +0530

    code_corection_added

--- palindrom.js
+++ palindrom.js
@@ -3,5 +3,10 @@ let str = "naman";
 let new_str = ""
 
 for(let i=str.length; i>0; i--){
-    new_str = new_str + str[i];
+    new_str = new_str + str[i]+" ";
+
+}if(new_str==str){
+    console.log("it is plaindrome")
+}else{
+    console.log("it is not plaindrome")
 }
--- prime.js
+++ prime.js
@@ -3,5 +3,9 @@ let count = 0;
 for(let i=1; i<=num; i++){
     if(num % i == 0){
         count++;
+    }if(count==2){
+        console.log("it is prime")
+    }else{
+        console.log("it is not prime")
     }
 }
